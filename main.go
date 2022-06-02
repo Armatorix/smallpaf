f17b0d62964882ba000b7d50bbfaffb5d2aed8f7
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2022-06-02 22:12:17 +0200

    fix deps

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/Armatorix/smallpaf/config"
+	"github.com/Armatorix/smallpaf/smtp"
 	"github.com/labstack/echo/v4"
 )
 
@@ -14,9 +15,12 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	smtpClient := smtp.NewClient(cfg.Smtp)
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(cfg.Server.Address()))
+	log.Fatal(e.Start(cfg.Server.Address()))
+	log.Println(smtpClient)
 }
