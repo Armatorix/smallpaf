619551654c8ae5e68df6b27d8ce5e9d1ab38d31d
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2022-06-03 00:20:39 +0200

    email form

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import { FormControl, InputLabel, Input, FormHelperText, Button } from "@mui/material"
 function App() {
   return (
     <div className="App">
@@ -7,8 +7,38 @@ function App() {
         SmallPAF
       </header>
 
+      <form onSubmit={(e) => {
+        e.preventDefault();
+
+        fetch(window.location.origin, {
+          body: JSON.stringify({ email: e.target.email.value }),
+          cache: 'no-cache',
+          headers: {
+            'content-type': 'application/json'
+          },
+          method: 'POST',
+          mode: 'cors',
+          redirect: 'follow',
+        })
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (myJson) {
+            console.log(myJson);
+          });
+        // TODO redirect on success
+      }}>
+        <FormControl >
+          <InputLabel htmlFor="email">Email</InputLabel>
+          <Input id="email" aria-describedby="email-helper" required />
+          <FormHelperText id="email-helper"></FormHelperText>
+          <Button type="submit">
+            Send auth email
+          </Button>
+        </FormControl>
+      </form>
     </div>
   );
 }
 
-export default App;
+export default App; 
