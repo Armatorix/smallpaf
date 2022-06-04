4fee1a0ea1e6ab8a0996ae8f28b233b0fbecb1b2
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2022-06-04 11:26:14 +0200

    new user page

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "@emotion/react";
 import {
   AppBar,
-  CssBaseline, Toolbar,
+  CssBaseline, Grid, Paper, Toolbar,
   Typography
 } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
@@ -17,18 +17,41 @@ function App() {
       <CssBaseline />
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" noWrap component="div" flexGrow={1}>
+          <Typography variant="h5" noWrap component="div" flexGrow={1}>
             SmallPAF - Planning Async Format
           </Typography>
           <ThemeToggle />
         </Toolbar>
       </AppBar>
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<NewUser />} />
-        </Routes>
-      </BrowserRouter>
+      <Grid
+        container
+        alignSelf="center"
+        alignItems="center"
+        minWidth="650px"
+        width="100%"
+        height="100%"
+        paddingY="3em"
+        justifyContent="center"
+      >
+        <Grid
+          container
+          item
+          component={Paper}
+          direction="column"
+          spacing={2}
+          width="90%"
+          maxWidth="1200px"
+          padding="2em"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<NewUser />} />
+            </Routes>
+          </BrowserRouter>
+        </Grid>
+      </Grid>
     </ThemeProvider>
   );
 }
--- web/src/routes/NewUser.js
+++ web/src/routes/NewUser.js
@@ -1,8 +1,13 @@
 import { ENDPOINT } from "../config.js"
-import { FormControl, InputLabel, Input, FormHelperText, Button } from "@mui/material"
+import { FormControl, InputLabel, Input, FormHelperText, Button, Typography } from "@mui/material"
 
 export default function NewUser() {
     return <div className="App">
+        <Typography variant="h4">Login with email</Typography>
+        <Typography variant="body1">
+            The email with the authentication link will be provided to your email.
+            No passwords - just open the link and start planning your sprints!
+        </Typography>
         <form onSubmit={(e) => {
             e.preventDefault();
 
