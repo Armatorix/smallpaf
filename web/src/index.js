d80408942bb9d3e9a11f55f84d68957ef134741e
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2022-06-04 11:05:58 +0200

    remove unused

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { RecoilRoot } from "recoil"
 
@@ -16,8 +14,3 @@ root.render(
     </BrowserRouter>
   </RecoilRoot>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
