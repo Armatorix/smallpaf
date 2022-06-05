466601208d58c15f13637e62a68652c402a7ff27
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2022-06-05 11:58:34 +0200

    cleanup theme

--- web/src/components/NewRoomButton.js
+++ web/src/components/NewRoomButton.js
@@ -1,11 +1,8 @@
 import AddIcon from "@mui/icons-material/Add";
 import { Button } from "@mui/material";
-import { purple } from "@mui/material/colors";
 import { styled } from "@mui/material/styles";
-import { color, palette } from "@mui/system";
 import * as React from "react";
 const ColorButton = styled(Button)(({ theme }) => {
-	console.log(theme);
 	return {
 		height: "4em",
 		borderColor: theme.palette.grey[600],
