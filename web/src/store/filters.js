7fb725de3b528e4a5bcfd1b6c1aae7d9f416b5f3
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2022-06-11 19:27:01 +0200

    filters recoil

---
+++ web/src/store/filters.js
@@ -0,0 +1,37 @@
+import { atom, useRecoilState } from "recoil";
+
+const hideVotedLocalStorageKey = "hideVoted";
+const hideSubmittedLocalStorageKey = "hideSubmitted";
+
+export const hideVotedState = atom({
+	key: "hideVotedState",
+	default:
+		localStorage.getItem(hideVotedLocalStorageKey) !== null
+			? localStorage.getItem(hideVotedLocalStorageKey)
+			: false,
+});
+export const hideSubmittedState = atom({
+	key: "hideSubmittedState",
+	default:
+		localStorage.getItem(hideSubmittedLocalStorageKey) !== null
+			? localStorage.getItem(hideSubmittedLocalStorageKey)
+			: false,
+});
+
+export const useHideVoted = () => {
+	const [hideVoted, set] = useRecoilState(hideVotedState);
+	const setHideVoted = (newValue) => {
+		set(newValue);
+		localStorage.setItem(hideVotedLocalStorageKey, newValue);
+	};
+	return [hideVoted, setHideVoted];
+};
+
+export const useHideSubmitted = () => {
+	const [hideSubmitted, set] = useRecoilState(hideSubmittedState);
+	const setHideSubmitted = (newValue) => {
+		set(newValue);
+		localStorage.setItem(hideSubmittedLocalStorageKey, newValue);
+	};
+	return [hideSubmitted, setHideSubmitted];
+};
