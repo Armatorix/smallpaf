fa2a9a772effafa5361dccdd32c4bf4d7b822bf6
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2022-06-11 17:55:07 +0200

    jira related code

--- handlers/ticket.go
+++ handlers/ticket.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/Armatorix/smallpaf/jira"
 	"github.com/Armatorix/smallpaf/model"
 	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
@@ -181,6 +182,72 @@ func (ch *CrudHandler) ResetVoting(c echo.Context) error {
 		return tx.Model(&ticket).Update("revealed", false).Error
 
 	})
+	if err != nil {
+		return err
+	}
+	return c.NoContent(http.StatusOK)
+}
+
+type requestApplyVotingToJira struct {
+	TicketId uuid.UUID `param:"ticketId" validate:"required"`
+	RoomID   uuid.UUID `param:"roomId" validate:"required"`
+	Points   int       `json:"Points"`
+}
+
+func (ch *CrudHandler) ApplyVotingToJira(c echo.Context) error {
+	var req requestApplyVotingToJira
+	if err := c.Bind(&req); err != nil {
+		return err
+	}
+
+	if err := c.Validate(req); err != nil {
+		return err
+	}
+
+	uid, err := getUID(c)
+	if err != nil {
+		return err
+	}
+	email, err := getEmail(c)
+	if err != nil {
+		return err
+	}
+
+	hasRights, err := ch.haveAccessToTheTicket(uid, req.TicketId)
+	if err != nil {
+		return err
+	}
+	if !hasRights {
+		return c.NoContent(http.StatusUnauthorized)
+	}
+
+	hasRights, usersRoom, err := ch.hasRoomAdminRights(uid, req.RoomID)
+	if err != nil {
+		return err
+	}
+	if !hasRights {
+		return c.NoContent(http.StatusUnauthorized)
+	}
+	room := model.Room{ID: req.RoomID}
+	err = ch.dbClient.First(&room).Error
+	if err != nil {
+		return err
+	}
+	ticket := model.Ticket{ID: req.TicketId}
+	err = ch.dbClient.First(&ticket).Error
+	if err != nil {
+		return err
+	}
+
+	if err != nil {
+		return err
+	}
+
+	jiraClient, err := jira.NewClient(email, usersRoom.JiraToken, room.JiraUrl)
+	if err != nil {
+		return err
+	}
+	err = jiraClient.SetTicketPoints(ticket.JiraID, req.Points)
 	if err != nil {
 		return err
 	}
---
+++ jira/jira.go
@@ -0,0 +1,73 @@
+package jira
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+)
+
+var errFailedStatusCode = errors.New("failed status code")
+
+type Client struct {
+	auth    string
+	jiraURL *url.URL
+}
+
+func NewClient(user, pass string, jiraURLStr string) (*Client, error) {
+
+	u, err := url.Parse(jiraURLStr)
+	if err != nil {
+		return nil, err
+	}
+	return &Client{
+		auth:    base64.StdEncoding.EncodeToString([]byte(user + ":" + pass)),
+		jiraURL: u,
+	}, nil
+}
+
+type jiraUpdateStoryPointsRequest struct {
+	Fields map[string]any `json:"fields"`
+}
+
+func (c *Client) SetTicketPoints(ticketId string, points int) error {
+	reqURL := url.URL{
+		Scheme: c.jiraURL.Scheme,
+		Host:   c.jiraURL.Host,
+		Path:   fmt.Sprintf("/rest/api/2/issue/%s", ticketId),
+	}
+
+	requestBody := jiraUpdateStoryPointsRequest{
+		Fields: map[string]any{
+			"customfield_10026": points,
+		},
+	}
+
+	var bodyBuf bytes.Buffer
+	if err := json.NewEncoder(&bodyBuf).Encode(requestBody); err != nil {
+		return err
+	}
+
+	r, err := http.NewRequest(http.MethodPut, reqURL.String(), &bodyBuf)
+	if err != nil {
+		return err
+	}
+	r.Header.Add("Content-Type", "application/json")
+	r.Header.Add("Authorization", "Basic "+c.auth)
+
+	resp, err := http.DefaultClient.Do(r)
+	if err != nil {
+		return err
+	}
+	if resp != nil && resp.Body != nil {
+		resp.Body.Close()
+	}
+
+	if resp.StatusCode >= 300 {
+		return errFailedStatusCode
+	}
+	return nil
+}
